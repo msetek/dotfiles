039a459629a9d8ac4d12b96c5664b41371da01a7
Martin Setek <martin@setek.org>
2018-02-28 04:42:38 +0100

    java project generator

---
+++ profile.d/java.sh
@@ -0,0 +1,19 @@
+java-skeleton-project() {
+    if [[ $# != 1 ]]; then
+        return 1
+    fi
+
+    local appname="$1"
+    local appnamelc=$(echo -n "$appname" | tr A-Z a-z)
+
+    if hash mvn 2> /dev/null; then
+        mvn archetype:generate \
+            -DgroupId="no.setek.$appnamelc" \
+            -DartifactId=$appname \
+            -DarchetypeArtifactId=maven-archetype-quickstart \
+            -DinteractiveMode=false
+
+    else
+        echo "maven is not installed"
+    fi
+}

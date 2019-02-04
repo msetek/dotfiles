68d17c1f40e3812daeff049895b3f549a145e5b4
Martin Setek <martin@setek.org>
2019-02-04 10:50:50 +0100

    add comment about how to configure liquidprompt

--- profile.d/liquidprompt.sh
+++ profile.d/liquidprompt.sh
@@ -1,3 +1,5 @@
+# https://github.com/nojhan/liquidprompt
+# configure with ~/.config/liquidpromptrc
 # only run liquidprompt for interactive shells
 if [[ -f /usr/local/share/liquidprompt && $- = *i* ]]; then
     . /usr/local/share/liquidprompt

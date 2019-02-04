37d6be7ebf1ef24bca6ced3dc9d915f63ecfbf3c
Martin Setek <martin@setek.org>
2019-02-04 10:52:06 +0100

    add function to create favicons

--- profile.d/msetek.sh
+++ profile.d/msetek.sh
@@ -99,3 +99,18 @@ top10() {
         nl |
         head -n10
 }
+
+favicon() {
+    convert "$1"  -background white \
+            \( -clone 0 -resize 16x16 -extent 16x16 \) \
+            \( -clone 0 -resize 32x32 -extent 32x32 \) \
+            \( -clone 0 -resize 48x48 -extent 48x48 \) \
+            \( -clone 0 -resize 64x64 -extent 64x64 \) \
+            -delete 0 -alpha off -colors 256 favicon.ico
+}
+
+alias l='ls -1aF '
+alias b='bat --plain '
+alias bb='bat '
+alias agg='ag --ignore node_modules --ignore dist '
+
